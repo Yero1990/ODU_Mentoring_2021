b01a8aae35908f0d2af1b22e16c7306b4be3ef99
Carlos Yero <cyero@jlab.org>
2021-07-27 19:14:00 -0400

    updated code

--- Exercises/Week1/coding_neuron.py
+++ Exercises/Week1/coding_neuron.py
@@ -24,15 +24,15 @@ class Neuron:
         attributes and methods can be initialized and accessed later on.
     '''
     def __init__(self, weights, bias):
-        print('Calling Neuron() __init__ function . . .')
+        #print('Calling Neuron() __init__ function . . .')
         self.weights = weights
         self.bias    = bias
 
-        print('weights = ', weights)
-        print('bias = ', bias)
+        #print('weights = ', weights)
+        #print('bias = ', bias)
 
     def feedforward(self, inputs):
-        print('Calling Neuron() feedforward . . .')
+        #print('Calling Neuron() feedforward . . .')
         # Weight the inputs, add bias, then use activation function
         # e.g. total = w1*x1 + w2*x2 + bias
         total = np.dot(self.weights, inputs) + self.bias
@@ -72,7 +72,7 @@ class OurNeuralNetwork:
         weights = np.array([0,1])
         bias = 0
 
-        # Call the Neuron Class to instantiate 2 hidden layer neurons and 1 output neuron layer
+        # Initialize an instance of the Neuron Class for 2 hidden layers and 1 output layer
         self.h1 = Neuron(weights, bias)
         self.h2 = Neuron(weights, bias)
         self.o1 = Neuron(weights, bias)
@@ -93,4 +93,83 @@ network = OurNeuralNetwork()
 # define the inputs to the neural network (i.e., data)
 x = np.array([2,3])
 
-print(network.feedforward(x))
+#print(network.feedforward(x))
+
+
+#--------------------------------------------
+# 3. Training a Neural Network, Part 1
+#--------------------------------------------
+
+'''
+ Say we have the following measurements we can train to predict someone's gender given their weight/height
+
+  Name      Weight(lb)   Height(in)    Gender
+-------------------------------------------------
+  Alice     133          65            F
+  Bob       160          72            M
+  Charlie   152          70            M
+  Diana     120          60            F
+
+
+Definitions
+Loss: Before we train our network, we first need a way to quantify how "good" it's doing
+so that it can try to do "better". That's what loss is.
+
+mean squared error (MSE) loss:
+
+  MSE = 1 / n * SUM_(i=1)->(i=n) ( y_true - y_pred)^2
+
+n      : number of samples, which is 4 (Alice, Bob, Charlie, Diana)
+y      : represents the variable being predicted, which is Gender in this case
+y_true : is the true value of the variable (the "correct answer"), for example, y_true for Alice would be 1
+y_pred : is the predicted value of the variable. It's whatever our networks outputs.
+
+(y_true - y_pred)^2 is the squared error, and the Loss function is the average over all squared errors
+
+Better predictions by Neural Network = Lower loss
+
+Training a network = trying to minimize its loss 
+
+'''
+
+# put the mean squared error into code
+def mse_loss(y_true, y_pred):
+    # y_true and y_pred are numpy arrays of the same length
+    mse = ((y_true - y_pred)**2).mean()
+    return mse
+
+y_true = np.array([1, 0, 0, 1])
+y_pred = np.array([0, 0, 0, 0])
+
+# print(mse_loss(y_true, y_pred)) # 0.5
+
+
+#--------------------------------------------
+# 4. Training a Neural Network, Part 2
+#--------------------------------------------
+
+'''
+ Goal: minimize the loss of the neural network (i.e., improve its predictive power)
+ This can be done by changing the network's weights and biases to influence its predictions,
+ but how do we do so in a way that decreases loss? Use multivariate calculus to minimize the loss
+
+ L (weights, bias) :  take partial derivatives of loss with respect to weights, biases and set to zero to
+ determine which combination of (weights, biases) minimizes the loss, i.e., parameter optimization. An example
+ of the math is done on the actual blog by Victor Zhou.
+
+ To optimize the loss function, the "Stochaistic Gradient Descent" method is used, which tells us how
+ to change the weights and biases to minimize loss, via the simple replacement of the current weight by:
+
+ w --> w - n * dL/dw, where n is a constant called the learning rate. All we're doing is subtracting n * dL/dw from w:
+
+ 1. If dL/dw > 0, w will decrease, which makes L decrease
+ 2. If dL/dw < 0, w will increase, which makes L decrease
+
+ This process of subtracting n * dL/dw from w is done iteratively such that the loss will eventually approximate zero,
+ which is directly related to the success of the neural network training
+
+
+
+
+
+'''
---
+++ Exercises/Week1/final_NN.py
@@ -0,0 +1,8 @@
+'''
+
+An Introduction to Neural Networks
+
+This code is the complete Neural Network following the simple example
+from the blog written by Victor Zhou: https://victorzhou.com/blog/intro-to-neural-networks/
+
+'''
