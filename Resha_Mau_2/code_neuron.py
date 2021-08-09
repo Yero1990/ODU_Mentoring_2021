fc2f8d03b9799ed83cc7d52a13793bd6826bbf7c
avcmau <resha.mau@ucalgary.ca>
2021-08-09 00:22:03 -0600

    Modified code_neuron.py to include the full code from the first example and deleted hello_world.py and hello_word.py~

--- Resha_Mau_2/code_neuron.py
+++ Resha_Mau_2/code_neuron.py
@@ -1,15 +1,27 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# Following along with Victor Zhou's tutorial series on neural networks
+# https://victorzhou.com/blog/intro-to-neural-networks/
 
+import numpy as np
 
 def sigmoid(x):
-    # Our activation function: f(x) = 1 / (1 + e^(-x))
-
+    # Activation function: f(x) = 1 / [1 + exp(-x)]
     return 1 / (1 + np.exp(-x))
 
-x = np.linspace(-10, 10, 100)
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
 
-# print(x)
+    def feedForward(self, inputs):
+        # Weigh inputs, add bias, then use the activation function
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
 
-plt.plot(x, sigmoid(x), color='r', linestyle='--')
-plt.show()
+weights = np.array([0, 1])      # w_1 = 0, w_2 = 1
+bias = 4                        # b = 4
+
+n = Neuron(weights, bias)
+
+x = np.array([2, 3])            # x_1 = 2, x_2 = 3
+
+print(n.feedforward(x))         # 0.9990889488055994
